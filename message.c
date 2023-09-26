a7bd05af58d1b9649e48a072b4d7b765157f5caf
Neil Martino <nbmartino@gmail.com>
2023-09-26 11:43:22 +0800

    with task list

--- message.c
+++ message.c
@@ -16,7 +16,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 */
 
 int
-match( char *string, char *pattern)
+match( char *str, char *pattern)
 {
   
       int status;
@@ -26,22 +26,25 @@ match( char *string, char *pattern)
           printf("reg compile error! status: %d\n", status);
            //return(0);      /* report error */
       }
+      printf("after compile\n");
       int iter = 0;
       do{
           iter++;
-        status = regexec(&re, "MID:123 CMD:START MODE:DEBUG FOO:bar baz\n", (size_t) 4, matches, 0);
+        status = regexec(&re, str, (size_t) 4, matches, 0);
         /* Print key-value pair */ 
-        char *key = &string[matches[1].rm_so];
-        char *value = &string[matches[1].rm_eo];
+        char *key = &str[matches[1].rm_so];
+        char *value = &str[matches[1].rm_eo];
         printf("\niter: %d %s=%s\n", iter, key, value);
         for(size_t m = 0; m < 4; m++){
-            printf("\nm: %d s:%d,e:%d\n", m, matches[m].rm_so, matches[m].rm_eo);
+            printf("\nm: %zu s:%lld,e:%lld\n", m, matches[m].rm_so, matches[m].rm_eo);
         }
     
       } while (status == 0);
       
       regfree(&re);
+      printf("status: %d", status);
       if (status != 0) {
+
             return(0);      /* report error */
       }
       return 0;
@@ -49,6 +52,11 @@ match( char *string, char *pattern)
 
 int main()
 {
+printf("main\n");
+
+// check message format
+// parse message
+// call script associated to command
  int result = match("MID:123 CMD:START MODE:DEBUG FOO:bar baz", "/^MID:\\w+\\s+CMD:\\w+\\s+\\b[A-Z]{2,}\\b:[\\w\\s]+(?:\\s+\\b[A-Z]{2,}\\b:[\\w\\s]+)*\\\\n|$/gm" );
  printf("result == %d", result);
   return 0;
